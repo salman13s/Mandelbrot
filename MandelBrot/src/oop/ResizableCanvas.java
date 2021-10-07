e49ce216cc3e644b69d9fc0e8210bed0791b1d2e
Salman Salman <ss79mite@studserv.uni-leipzig.de>
2021-10-07 22:12:28 +0300

    update

---
+++ MandelBrot/src/oop/ResizableCanvas.java
@@ -0,0 +1,11 @@
+package oop;
+
+import javafx.scene.canvas.Canvas;
+
+public class ResizableCanvas extends Canvas {
+
+    @Override
+    public boolean isResizable() {
+        return true;
+    }
+}
